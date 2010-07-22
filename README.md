d47477eec97a0f0b3f841fa39e0f302e7ef62a53
Samuel Spiza <sam.spiza@gmail.com>
2010-07-22 10:57:57 +0200

    Added options for in/output paths, Readme, Unlicense

---
+++ README.md
@@ -0,0 +1,9 @@
+updatenotifier
+==============
+
+This script helps you keep track of software updates. Usefull for software
+that doesen't have autoupdate or for software you don't use regular enough to
+use autoupdate.
+
+[Updatenotifier](http://github.com/samuelspiza/updatenotifier) is hosted on
+Github.
--- updatenotifier.py
+++ updatenotifier.py
@@ -1,7 +1,29 @@
 #!/usr/bin/env python
 #
-# Copyright (c) 2010, Samuel Spiza
-# All rights reserved.
+# This is free and unencumbered software released into the public domain.
+# 
+# Anyone is free to copy, modify, publish, use, compile, sell, or
+# distribute this software, either in source code form or as a compiled
+# binary, for any purpose, commercial or non-commercial, and by any
+# means.
+# 
+# In jurisdictions that recognize copyright laws, the author or authors
+# of this software dedicate any and all copyright interest in the
+# software to the public domain. We make this dedication for the benefit
+# of the public at large and to the detriment of our heirs and
+# successors. We intend this dedication to be an overt act of
+# relinquishment in perpetuity of all present and future rights to this
+# software under copyright law.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+# 
+# For more information, please refer to <http://unlicense.org/>
 #
 """Update Notifier
 
@@ -9,15 +31,24 @@ A script that checks if updated versions of software is available for download.
 """
 
 __author__ = "Samuel Spiza <sam.spiza@gmail.com>"
-__copyright__ = "Copyright (c) 2010, Samuel Spiza"
-__version__ = "0.1"
+__license__ = "Public Domain"
+__version__ = "0.1.1"
 
 import re
 import os
 import json
 import urllib
 import urllib2
+import optparse
 import sys
+
+JSON_FILE = "updatenotifier.json"
+LOG_FILE = "updatenotifer.log"
+OUTPUT_FILE = "updatenotifications.txt"
+
+FAILED     = "{0:{1}} {2:{3}} No Match."
+UPDATE     = "{0:{1}} {2:{3}} Version {4} available."
+UP_TO_DATE = "{0:{1}} {2:{3}}"
 
 opener = urllib2.build_opener(urllib2.HTTPCookieProcessor())
 urllib2.install_opener(opener)
@@ -26,9 +57,15 @@ HEADER = {'User-Agent': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0)',
           'Accept-Language': 'de',
           'Accept-Encoding': 'utf-8'}
 
-FAILED     = "{0:{1}} {2:{3}} No Match."
-UPDATE     = "{0:{1}} {2:{3}} Version {4} available."
-UP_TO_DATE = "{0:{1}} {2:{3}}"
+def getOptions(argv):
+    parser = optparse.OptionParser()
+    parser.add_option("-o", "--output",
+                      dest="output", metavar="PATH", default=None,
+                      help="Change the path of the output file.")
+    parser.add_option("-i", "--input",
+                      dest="input", metavar="PATH", default=None,
+                      help="Change the path of the input file.")
+    return parser.parse_args(argv)[0]
 
 def getResponse(url, postData=None):
     if(postData is not None):
@@ -60,9 +97,12 @@ def check(name, url, regexp, current, nameLen, versionLen):
         out = UP_TO_DATE.format(name, nameLen, current, versionLen)
         return out, ""
 
-def main():
+def main(argv):
+    options = getOptions(argv)
     printlog = False
-    file = open("updatenotifier.json", "r")
+    if options.input is not None:
+        JSON_FILE = options.input
+    file = open(JSON_FILE, "r")
     toolsToCheck = json.loads(file.read())
     file.close()
     nameLen = max([len(i['name']) for i in toolsToCheck])
@@ -77,14 +117,16 @@ def main():
         output += out
         logout += log + "\n"
     if printlog:
-        file = open("updatenotifer.log", "w")
+        file = open(LOG_FILE, "w")
         file.write(logout)
         file.close()
     if 0 < len(output):
-        file = open("updatenotifications.txt", "w")
+        if options.output is not None:
+            OUTPUT_FILE = options.output
+        file = open(OUTPUT_FILE, "w")
         file.write(output)
         file.close()
     return 0
 
 if __name__ == "__main__":
-    sys.exit(main())
+    sys.exit(main(sys.argv[1:]))
